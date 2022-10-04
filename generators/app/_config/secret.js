46b99c42f33b0079b21407ad40cf3613e5169a98
gaelazzo <gaetano.lazzo@gmail.com>
2022-10-04 16:20:56 +0200

    w.i.p.

--- generators/app/_config/secret.js
+++ generators/app/_config/secret.js
@@ -3,5 +3,5 @@ const Crypto = require('crypto-js');
 module.exports = {
     key: Crypto.enc.Hex.parse('0001020304050607'),
     iv: Crypto.enc.Hex.parse('08090a0b0c0d0e0f'),
-    pwd: 'abs!sçlds28aqqq'
+    pwd: 'ebq!sçlas34aqw9'
 }
