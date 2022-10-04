aaf823317dc2abd35a9a227d5bb7b8f5879bf680
gaelazzo <gaetano.lazzo@gmail.com>
2022-10-04 16:16:19 +0200

    w.i.p.

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,5 +1,7 @@
 const Generator = require('yeoman-generator');
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
+const _ = require("lodash");
 
 module.exports = class extends Generator {
     // The name `constructor` is important here
@@ -11,9 +13,27 @@ module.exports = class extends Generator {
         this.log(args);
         this.log(opts);
     }
-    async installingJsMetaBackend() {
-        await this.addDependencies(['jsmetabackend'], { 'save-dev': true });
+
+    updateDependencies(){
+        const pkgJson = {
+            dependencies: {
+                "lodash": "4.17.21",
+                "jquery": "1.7.4"
+            },
+            devdependencies: {
+                "jasmine-core": "~2.2.1"
+            }
+        };
+
+        // Extend or create package.json file in destination path
+        this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
     }
+
+    //
+    // async installingJsMetaBackend() {
+    //     await this.addDependencies(['jsmetabackend'], { 'save-dev': true });
+    // }
+
     _checkCreateFolder(folderName){
         if (!fs.existsSync(folderName)){
             fs.mkdirSync(folderName);
@@ -27,8 +47,31 @@ module.exports = class extends Generator {
     init() {
         const folders=["client",
             "client/components",
-            "client/datasets"];
+            "client/datasets",
+            "routes",
+            "meta"
+        ];
         folders.forEach(f=> this._checkCreateFolder(f));
+
+        //_.extend(Generator.prototype, require('yeoman-generator/lib/actions/install'));
+
+        // this.installDependencies({
+        //     bower: true,
+        //     npm: true
+        // });
+
+
+        let packagePath = path.dirname(require.resolve("jsmetabackend/package.json"));
+
+        console.log("Copying from "+path.join(packagePath,"routes"));
+        fs.cp(path.join(packagePath,"routes"),"routes",
+            { recursive: true },
+            (err) => {
+                if (err) {
+                    console.error(err);
+                }
+            });
+
     }
 
 
